88380a72931231220f057432f0fb6cc73b5e60d4
Royce Remulla <royce.com@gmail.com>
2016-10-30 13:37:38 +0800

    old dev codes

--- html_scraper/wikipedia_senators.js
+++ html_scraper/wikipedia_senators.js
@@ -1,12 +1,14 @@
 /**
- * Get state, name, dob of US Senators.
+ * Get state, name, dob, class of US Senators.
  */
 
-var output = '';
-$('.wikitable.sortable tbody tr').each(function() {
-    var $state = $(this).find('a:first');
-    var $name = $(this).find('.fn a');
-    var dob = $(this).find('.bday');
-	output += "'" + $state.text() + "', '" + $name.text() + "', date '" + dob.text() + "'\n";
-});
-alert(output);
+ var output = '';
+ $('.wikitable.sortable tbody tr').each(function() {
+     var $this = $(this)
+     var $state = $this.find('a:first');
+     var $name = $this.find('.fn a');
+     var dob = $this.find('.bday');
+     var cls = $this.find('td:eq(2)').text()
+ 	output += "'" + $state.text() + ", " + $name.text() + ", " + dob.text() + ", " + cls +"\n";
+ });
+ alert(output);
